4c7e14740ba6598c862c1dad914f5b4e31436e89
anshcena <anshcena.1996@gmail.com>
2020-09-17 16:03:56 +0530

    (complete_details: added)

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
                 <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Project</a></li>
                 <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
                 <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
-                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Contact</a></li>
             </ul>
         </div>
     </nav>
@@ -31,7 +31,7 @@ function App() {
                     <span class="text-primary">Varun</span>
                 </h1>
                 <div class="subheading mb-5">
-                    Lucknow · UP · INDIA·
+                    Lucknow · UP · INDIA· (+91)-8896447683 
                     <a href="mailto:anshcena.1996@gmail.com">anshcena.1996@gmail.com</a>
                 </div>
                 <p class="lead mb-5">I help ventures to transform their business to perform better, easy to operate, provide the security in business and automating their modules.</p>
@@ -75,36 +75,35 @@ function App() {
                 <h2 class="mb-5">Project</h2>
                 <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
                     <div class="flex-grow-1">
-                        <h3 class="mb-0">Senior Web Developer</h3>
-                        <div class="subheading mb-3">Intelitec Solutions</div>
-                        <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
+                        <h3 class="mb-0">MldDroid</h3>
+                        <div class="subheading mb-3"><a href="https://play.google.com/store/apps/details?id=in.mltool.mldroid&hl=en_IN">Link</a></div>  
+                        <p>Firebase powered Android app developed using ML Kit. This app demonstrates various Machine Learning models like face detection, text detection, barcode scanning, labelling images, and identifying text from images. All the features can be explored using camera in real time.</p>
                     </div>
-                    <div class="flex-shrink-0"><span class="text-primary">March 2013 - Present</span></div>
+                    {/* <div class="flex-shrink-0"><span class="text-primary">March 2013 - Present</span></div> */}
                 </div>
                 <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
                     <div class="flex-grow-1">
-                        <h3 class="mb-0">Web Developer</h3>
-                        <div class="subheading mb-3">Intelitec Solutions</div>
-                        <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+                        <h3 class="mb-0">Wall App</h3>
+                        <div class="subheading mb-3"><a href="https://play.google.com/store/apps/details?id=com.preflet.ami&hl=en_IN">Link</a></div>                        <p>"WALL: Breaking Walls. Building Futures" is a project developed by AMI, which was selected to represent the "No More Bricks in the WALL" campaign in Portugal.
+Answer the quiz and try to guess the nationality of each person portrayed. Use the app to check your knowledge. We believe you will have some surprises!</p>
                     </div>
-                    <div class="flex-shrink-0"><span class="text-primary">December 2011 - March 2013</span></div>
+                    {/* <div class="flex-shrink-0"><span class="text-primary">December 2011 - March 2013</span></div> */}
                 </div>
                 <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
                     <div class="flex-grow-1">
-                        <h3 class="mb-0">Junior Web Designer</h3>
-                        <div class="subheading mb-3">Shout! Media Productions</div>
-                        <p>Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.</p>
+                        <h3 class="mb-0">DCR APP</h3>
+                        <div class="subheading mb-3"><a href="https://dcr.auroswift.com/">Link</a></div>
+                        <p>Daily Call Report Web App | PWA build to file daily call report / work / performance which performs productivity checkup and monthly monitoring of tasks </p>
                     </div>
-                    <div class="flex-shrink-0"><span class="text-primary">July 2010 - December 2011</span></div>
                 </div>
                 <div class="d-flex flex-column flex-md-row justify-content-between">
                     <div class="flex-grow-1">
-                        <h3 class="mb-0">Web Design Intern</h3>
-                        <div class="subheading mb-3">Shout! Media Productions</div>
-                        <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
+                        <h3 class="mb-0">COVIA</h3>
+                        <div class="subheading mb-3"><a href="https://covia.robomx.tech/">Link</a></div>
+                        <p>Corona Virus Alert app containing informative real data of the outburst.</p>
                     </div>
-                    <div class="flex-shrink-0"><span class="text-primary">September 2008 - June 2010</span></div>
                 </div>
+                <div class="subheading mb-3"><a href="https://github.com/anshcena">For More Work, Checkout my Github!</a></div>
             </div>
         </section>
         <hr class="m-0" />
@@ -120,14 +119,12 @@ function App() {
                     <li class="list-inline-item"><i class="fab fa-angular"></i></li>
                     <li class="list-inline-item"><i class="fab fa-react"></i></li>
                     <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-java"></i></li>
                     <li class="list-inline-item"><i class="fab fa-sass"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-less"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-gulp"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-grunt"></i></li>
                     <li class="list-inline-item"><i class="fab fa-npm"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-github"></i></li>	
                 </ul>
-                <div class="subheading mb-3">Workflow</div>
+                {/* <div class="subheading mb-3">Workflow</div>
                 <ul class="fa-ul mb-0">
                     <li>
                         <span class="fa-li"><i class="fas fa-check"></i></span>
@@ -145,7 +142,7 @@ function App() {
                         <span class="fa-li"><i class="fas fa-check"></i></span>
                         Agile Development & Scrum
                     </li>
-                </ul>
+                </ul> */}
             </div>
         </section>
         <hr class="m-0" />
@@ -153,53 +150,32 @@ function App() {
         <section class="resume-section" id="interests">
             <div class="resume-section-content">
                 <h2 class="mb-5">Interests</h2>
-                <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
-                <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+                <p>Apart from being a Frontend developer, tech that interests me is AR, ML, Blockchain Of things and IoT!</p>
+                <p class="mb-0">I love to travel frequently and explore the different regions. Living and understanding the different ascpects of life, culture and places.
+                Things that fascinate and make me curious is the Human Brain and <strong>Space.</strong></p>
             </div>
         </section>
         <hr class="m-0" />
         {/* <!-- Awards--> */}
         <section class="resume-section" id="awards">
             <div class="resume-section-content">
-                <h2 class="mb-5">Awards & Certifications</h2>
+                <h2 class="mb-5">Lets grab a coffee??<i class="fas fa-coffee text-danger"></i></h2>
                 <ul class="fa-ul mb-0">
                     <li>
-                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                        Google Analytics Certified Developer
+                        <span class="fa-li"><i class="fas fa-envelope text-danger"></i></span>
+                        Feel free to Mail : <a href="mailto:anshcena.1996@gmail.com">anshcena.1996@gmail.com</a>
                     </li>
                     <li>
-                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                        Mobile Web Specialist - Google Certification
+                        <span class="fa-li"><i class="fas fa-code text-warning"></i></span>
+                        Follow me on <a href="https://twitter.com/anshcasm">Twitter</a> and <a href="https://github.com/anshcena">Github</a> to follow my journey of <a href="https://www.100daysofcode.com/">#100daysOfCode Challenge</a>
                     </li>
                     <li>
-                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                        1
-                        <sup>st</sup>
-                        Place - University of Colorado Boulder - Emerging Tech Competition 2009
+                        <span class="fa-li"><i class="fab fa-twitter text-info"></i></span>
+                        <a href="https://twitter.com/anshcasm">Twitter</a> 
                     </li>
                     <li>
-                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                        1
-                        <sup>st</sup>
-                        Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)
-                    </li>
-                    <li>
-                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                        2
-                        <sup>nd</sup>
-                        Place - University of Colorado Boulder - Emerging Tech Competition 2008
-                    </li>
-                    <li>
-                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                        1
-                        <sup>st</sup>
-                        Place - James Buchanan High School - Hackathon 2006
-                    </li>
-                    <li>
-                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                        3
-                        <sup>rd</sup>
-                        Place - James Buchanan High School - Hackathon 2005
+                        <span class="fa-li"><i class="fab fa-github text-dark"></i></span>
+                        <a href="https://github.com/anshcena">Github</a> 
                     </li>
                 </ul>
             </div>
